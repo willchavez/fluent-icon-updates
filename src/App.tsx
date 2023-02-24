7ce4efe2a5e80cdf1efdd7bd4367a605ff4a0114
Will Chavez <williamch@microsoft.com>
2023-02-24 08:47:30 -0800

    Updates.

--- src/App.tsx
+++ src/App.tsx
@@ -1,129 +1,129 @@
-import React from "react"
-import "./App.css"
-import { IconList } from "./ui/IconList"
-import html2canvas from "html2canvas"
-import { Camera20Filled } from "@fluentui/react-icons"
+import React from 'react';
+import './App.css';
+import { IconList } from './ui/IconList';
+import html2canvas from 'html2canvas';
+import { Camera20Filled } from '@fluentui/react-icons';
 
 function App() {
-	let [files, setFiles] = React.useState([])
-	let [objectUrls, setObjectUrls] = React.useState([])
+	let [files, setFiles] = React.useState([]);
+	let [objectUrls, setObjectUrls] = React.useState([]);
 
-	let __addDirectory = (node: any) => {}
+	let __addDirectory = (node: any) => {};
 
 	let __onUploadhandler = (node: any) => {
-		let localFiles = node.target.files
+		let localFiles = node.target.files;
 
 		let filteredFiles = [...localFiles].filter(
-			(file: any) => file.name !== ".DS_Store"
-		)
+			(file: any) => file.name !== '.DS_Store'
+		);
 		const collator = new Intl.Collator(undefined, {
 			numeric: true,
-			sensitivity: "base",
-		})
+			sensitivity: 'base',
+		});
 
 		let locaFilesArr: any = Object.values(filteredFiles).sort(
 			(a: any, b: any) => {
-				return collator.compare(a.name, b.name)
+				return collator.compare(a.name, b.name);
 			}
-		)
-		setFiles(locaFilesArr)
+		);
+		setFiles(locaFilesArr);
 
-		let localObjectUrls: any = []
+		let localObjectUrls: any = [];
 		for (let i = 0; i < locaFilesArr.length; i++) {
-			localObjectUrls.push(URL.createObjectURL(locaFilesArr[i]))
+			localObjectUrls.push(URL.createObjectURL(locaFilesArr[i]));
 		}
-		setObjectUrls(localObjectUrls)
-	}
+		setObjectUrls(localObjectUrls);
+	};
 
 	let button = (
 		<button
-			type="button"
-			className="btn btn-primary"
-			style={{ marginLeft: "2rem", height: "35px" }}
+			type='button'
+			className='btn btn-primary'
+			style={{ marginLeft: '2rem', height: '35px' }}
 			onClick={() => {
 				html2canvas(
-					document.querySelector("#app") || new HTMLElement(),
+					document.querySelector('#iconList') || new HTMLElement(),
 					{
 						scale: 3,
 					}
 				).then((canvas: any) => {
 					var image = canvas
-						.toDataURL("image/png")
-						.replace("image/png", "image/octet-stream") // here is the most important part because if you dont replace you will get a DOM 18 exception.
+						.toDataURL('image/png')
+						.replace('image/png', 'image/octet-stream'); // here is the most important part because if you dont replace you will get a DOM 18 exception.
 
-					downloadImage(image, "fluent-updates-overview.jpeg")
+					downloadImage(image, 'fluent-updates-overview.jpeg');
 
 					function downloadImage(
 						data: string,
-						filename = "untitled.jpeg"
+						filename = 'untitled.jpeg'
 					) {
-						var a = document.createElement("a")
-						a.href = data
-						a.download = filename
-						document.body.appendChild(a)
-						a.click()
+						var a = document.createElement('a');
+						a.href = data;
+						a.download = filename;
+						document.body.appendChild(a);
+						a.click();
 					}
-				})
+				});
 			}}
 		>
 			<Camera20Filled />
 		</button>
-	)
+	);
 
 	return (
-		<div className="App">
-			<div id="app" style={{ paddingBottom: "1rem" }}>
-				<header className="App-header">
-					<div className="container">
-						<div className="row">
+		<div className='App'>
+			<div id='app' style={{ paddingBottom: '1rem' }}>
+				<header className='App-header'>
+					<div className='container'>
+						<div className='row'>
 							<h1>Fluent Icon Updates</h1>
 						</div>
-						<div className="row">
+						<div className='row'>
 							<h4>
 								Using @fluentui/react-icons@
 								{process.env.REACT_APP_FLUENT_ICONS_VERSION}
 							</h4>
 						</div>
-						<div className="row">
+						<div className='row'>
 							<h4>
 								{new Date().toDateString()} {button}
-							</h4>{" "}
+							</h4>{' '}
 						</div>
 					</div>
 				</header>
 
 				{objectUrls.length > 0 && (
-					<div style={{ paddingBottom: "2rem" }}></div>
+					<div style={{ paddingBottom: '2rem' }}></div>
 				)}
 
-				<div>
+				<div id='iconList'>
 					{objectUrls.length === 0 && (
-						<div style={{ paddingTop: "2rem" }}>
+						<div style={{ paddingTop: '2rem' }}>
 							<input
-								type="file"
-								id="dcmFolderSelector"
+								type='file'
+								id='dcmFolderSelector'
 								multiple
 								/* @ts-expect-error */
-								directory=""
-								webkitdirectory=""
+								directory=''
+								webkitdirectory=''
 								ref={__addDirectory}
 								onChange={__onUploadhandler}
-								accept="image/svg"
-								class="hidden"
+								accept='image/svg'
+								class='hidden'
 							/>
 							<label
-								htmlFor="dcmFolderSelector"
-								className="btn btn-primary"
+								htmlFor='dcmFolderSelector'
+								className='btn btn-primary'
 							>
 								Select your folder!
 							</label>
 						</div>
 					)}
-					<IconList fileList={objectUrls} files={files} />{" "}
+					<IconList fileList={objectUrls} files={files} />{' '}
 				</div>
 			</div>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
--- src/ui/IconList.tsx
+++ src/ui/IconList.tsx
@@ -1,175 +1,178 @@
-import * as React from "react"
-import { Icon } from "./Icon"
-import "./IconList.css"
-import { findSVGsInReactPackage } from "../api/fetchicons"
-import { addNewUpdated, sanitizeFileNames } from "../util/helpers"
+import * as React from 'react';
+import { Icon } from './Icon';
+import './IconList.css';
+import { findSVGsInReactPackage } from '../api/fetchicons';
+import { addNewUpdated, sanitizeFileNames } from '../util/helpers';
 
 export interface IconListProps {
-	fileList: any[]
-	files: any
+	fileList: any[];
+	files: any;
 }
 
 export const IconList = (props: IconListProps) => {
-	let changedFileNames = sanitizeFileNames(props.files)
-	let [fileNameTree, setFileNameTree]: any[] = React.useState({})
+	let changedFileNames = sanitizeFileNames(props.files);
+	let [fileNameTree, setFileNameTree]: any[] = React.useState({});
 
 	React.useEffect(() => {
 		async function fetchMyAPI() {
-			let data = await findSVGsInReactPackage(changedFileNames)
+			let data = await findSVGsInReactPackage(changedFileNames);
 
 			for (var i = 0; i < data.length; i++) {
 				if (data[i]) {
-					changedFileNames[i].status = "updated"
-					changedFileNames[i].component = data[i].module
-					changedFileNames[i].reactComponentName = data[i].name
+					changedFileNames[i].status = 'updated';
+					changedFileNames[i].component = data[i].module;
+					changedFileNames[i].reactComponentName = data[i].name;
 				} else {
-					changedFileNames[i].status = "new"
+					changedFileNames[i].status = 'new';
 				}
 			}
-			setFileNameTree(addNewUpdated(changedFileNames, props.fileList))
+			setFileNameTree(addNewUpdated(changedFileNames, props.fileList));
 			// setPreviousSVGs(data)
 		}
 
-		fetchMyAPI()
+		fetchMyAPI();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [JSON.stringify(props.fileList)])
+	}, [JSON.stringify(props.fileList)]);
 
 	let gridHeader = () => {
 		return (
-			<div className="row headerRow">
+			<div className='row headerRow'>
 				{/* <div className="col">Name</div>
 				<div className="col">Icon</div>
 				<div className="col">Regular/Filled Icons Overlapped</div>
 				<div className="col">Published Icon in Github</div>
 				<div className="col">Published/Current Icon Overlapped</div> */}
 
-				<div className="col">ICON NAME</div>
-				<div className="col">SIZE</div>
-				<div className="col">REGULAR</div>
-				<div className="col">FILLED</div>
-				<div className="col">DIFFERENCE</div>
-				<div className="col"></div>
+				<div className='col'>ICON NAME</div>
+				<div className='col'>SIZE</div>
+				<div className='col'>REGULAR</div>
+				<div className='col'>FILLED</div>
+				<div className='col'>DIFFERENCE</div>
+				<div className='col'></div>
 			</div>
-		)
-	}
+		);
+	};
 	return (
-		<div className="container main">
+		<div className='container main'>
 			{props.files.length > 0 && gridHeader()}
 
 			{Object.keys(fileNameTree).map((iconName: any, index: number) => {
 				if (
-					iconName.includes("_new") ||
-					iconName === "text direction horizontal left"
+					iconName.includes('_new') ||
+					iconName === 'text direction horizontal left'
 				)
-					return <div></div>
-				console.log(iconName)
-				let regularIcon = ""
-				let filledIcon = ""
+					return <div></div>;
+				console.log(iconName);
+				let regularIcon = '';
+				let filledIcon = '';
 
-				let typesAndSizes: any[] = []
-				let sizeKeys = Object.keys(fileNameTree[iconName])
+				let typesAndSizes: any[] = [];
+				let sizeKeys = Object.keys(fileNameTree[iconName]);
 				sizeKeys.map((size) => {
-					typesAndSizes.push(...fileNameTree[iconName][size])
-				})
+					typesAndSizes.push(...fileNameTree[iconName][size]);
+				});
 
 				for (let i = typesAndSizes.length - 1; i >= 0; i--) {
-					if (typesAndSizes[i].style === "regular" && !regularIcon) {
-						regularIcon = typesAndSizes[i].urlPath
+					if (typesAndSizes[i].style === 'regular' && !regularIcon) {
+						regularIcon = typesAndSizes[i].urlPath;
 					}
-					if (typesAndSizes[i].style === "filled" && !filledIcon)
-						filledIcon = typesAndSizes[i].urlPath
+					if (typesAndSizes[i].style === 'filled' && !filledIcon)
+						filledIcon = typesAndSizes[i].urlPath;
 				}
 
-				let previousSVGRegular = <></>
-				let previousSVGFilled = <></>
+				let previousSVGRegular = <></>;
+				let previousSVGFilled = <></>;
 
-				let currentSVGRegular = ""
+				let currentSVGRegular = '';
 
-				let currentSVGFilled = ""
-
-
-
-
-
+				let currentSVGFilled = '';
 
 				if (
-					!iconName.includes("_new") &&
-					fileNameTree[iconName][
-						`${sizeKeys[0]}`
-					].find((x: any) => x.style === "regular")
+					!iconName.includes('_new') &&
+					fileNameTree[iconName][`${sizeKeys[0]}`].find(
+						(x: any) => x.style === 'regular'
+					)
 				) {
 					previousSVGRegular = fileNameTree[iconName][
 						`${sizeKeys[0]}`
 					]
-						.find((x: any) => x.style === "regular")
-						.component({ title: "idk" })
+						.find((x: any) => x.style === 'regular')
+						.component({ title: 'idk' });
 
 					currentSVGRegular = fileNameTree[iconName][
 						`${sizeKeys[0]}`
-					].find((x: any) => x.style === "regular").urlPath
+					].find((x: any) => x.style === 'regular').urlPath;
 				}
 
 				if (
-					!iconName.includes("_new") &&
-					fileNameTree[iconName][
-						`${sizeKeys[0]}`
-					].find((x: any) => x.style === "filled")
+					!iconName.includes('_new') &&
+					fileNameTree[iconName][`${sizeKeys[0]}`].find(
+						(x: any) => x.style === 'filled'
+					)
 				) {
-					previousSVGFilled = fileNameTree[iconName][
-						`${sizeKeys[0]}`
-					]
-						.find((x: any) => x.style === "filled")
-						.component({ title: "idk" })
+					previousSVGFilled = fileNameTree[iconName][`${sizeKeys[0]}`]
+						.find((x: any) => x.style === 'filled')
+						.component({ title: 'idk' });
 
 					currentSVGFilled = fileNameTree[iconName][
 						`${sizeKeys[0]}`
-					].find((x: any) => x.style === "filled").urlPath
+					].find((x: any) => x.style === 'filled').urlPath;
 				}
 
+				console.log('stuff');
+
+				console.log(sizeKeys[0]);
+				console.log(previousSVGRegular);
+				console.log(currentSVGRegular);
+				console.log(previousSVGFilled);
+				console.log(currentSVGFilled);
+
 				return (
-					<div className="row">
-						<div className="col">
-							{iconName.includes("_new") ? (
-								<div className="newIcon">NEW</div>
+					<div className='row'>
+						<div className='col'>
+							{iconName.includes('_new') ? (
+								<div className='newIcon'>NEW</div>
 							) : (
-								<div className="updatedIcon">UPDATED</div>
+								<div className='updatedIcon'>UPDATED</div>
 							)}
-							<div className="iconLabel">
-								{iconName.includes("_new")
-									? iconName.replace("_new", "")
+							<div className='iconLabel'>
+								{iconName.includes('_new')
+									? iconName.replace('_new', '')
 									: iconName}
 							</div>
 						</div>
-						<div className="col">
-							{sizeKeys.join(", ")}
-						</div>
-						<div className="col">
+						<div className='col'>{sizeKeys.join(', ')}</div>
+						<div className='col'>
 							{regularIcon ? (
-								<Icon element={regularIcon} altText="" />
+								<Icon element={regularIcon} altText='' />
 							) : (
-								<div className="lineline"></div>
+								<div className='lineline'></div>
 							)}
 						</div>
-						<div className="col">
+						<div className='col'>
 							{filledIcon ? (
-								<Icon element={filledIcon} altText="" />
+								<Icon element={filledIcon} altText='' />
 							) : (
-								<div className="lineline"></div>
+								<div className='lineline'></div>
 							)}
 						</div>
 
-						<div className="col prevCurrentComparison">
-							{!iconName.includes("new") && regularIcon ? (
+						<div className='col prevCurrentComparison'>
+							{!iconName.includes('new') && regularIcon ? (
 								<div>
 									<div>
-										<Icon
-											element={currentSVGRegular}
-											altText=""
-											isCompare={true}
-										/>
+										{currentSVGRegular ? (
+											<Icon
+												element={currentSVGRegular}
+												altText=''
+												isCompare={true}
+											/>
+										) : (
+											<div></div>
+										)}
 									</div>
-									<div className="previousICONCompare">
-										{!iconName.includes("_new") ? (
+									<div className='previousICONCompare'>
+										{!iconName.includes('_new') ? (
 											previousSVGRegular
 										) : (
 											<div></div>
@@ -177,21 +180,25 @@ export const IconList = (props: IconListProps) => {
 									</div>
 								</div>
 							) : (
-								<div className="lineline move"></div>
+								<div className='lineline move'></div>
 							)}
 						</div>
-						<div className="col prevCurrentComparison ">
-							{!iconName.includes("new") && filledIcon ? (
+						<div className='col prevCurrentComparison '>
+							{!iconName.includes('new') && filledIcon ? (
 								<div>
 									<div>
-										<Icon
-											element={currentSVGFilled}
-											altText=""
-											isCompare={true}
-										/>
+										{currentSVGFilled ? (
+											<Icon
+												element={currentSVGFilled}
+												altText=''
+												isCompare={true}
+											/>
+										) : (
+											<div></div>
+										)}
 									</div>
-									<div className="previousICONCompare">
-										{!iconName.includes("_new") ? (
+									<div className='previousICONCompare'>
+										{!iconName.includes('_new') ? (
 											previousSVGFilled
 										) : (
 											<div></div>
@@ -199,7 +206,7 @@ export const IconList = (props: IconListProps) => {
 									</div>
 								</div>
 							) : (
-								<div className="lineline move"></div>
+								<div className='lineline move'></div>
 							)}
 						</div>
 						{/* <div className="col prevCurrentComparison">
@@ -221,79 +228,8 @@ export const IconList = (props: IconListProps) => {
 							)}
 						</div> */}
 					</div>
-				)
-				// let labelBoolean: boolean =
-				// 	index > 0 &&
-				// 	changedFileNames[index].name ===
-				// 		 [index - 1].name &&
-				// 	changedFileNames[index].size ===
-				// 		changedFileNames[index - 1].size
-				// let iconName = (
-				// 	<div className="iconLabel">
-				// 		{changedFileNames[index].name}{" "}
-				// 		{changedFileNames[index].size}{" "}
-				// 		{changedFileNames[index].style}
-				// 	</div>
-				// )
-				// let current = {
-				// 	element: el,
-				// 	altText: props.files[index],
-				// }
-				// let previous = {
-				// 	element: props.fileList[index + 1],
-				// 	altText: props.files[index + 1],
-				// }
-				// if (labelBoolean) {
-				// 	previous = {
-				// 		element: props.fileList[index - 1],
-				// 		altText: props.files[index - 1],
-				// 	}
-				// }
-				// return (
-				// 	<div className="row flex-grow-1">
-				// 		<div className="col nameLabel">{iconName}</div>
-				// 		<div className="col">
-				// 			<Icon element={el} altText={props.files[index]} />
-				// 		</div>
-				// 		<div className="col">
-				/* <OverlappingIcon
-								current={current}
-								previous={previous}
-							/> */
-				// 	</div>
-				// 	<div className="col">
-				// 		{previousSVGs.length > 0 && previousSVGs[index] ? (
-				// 			<div
-				// 				className="previousICON"
-				// 				dangerouslySetInnerHTML={{
-				// 					__html: previousSVGs[index],
-				// 				}}
-				// 			/>
-				// 		) : (
-				// 			<div>new</div>
-				// 		)}
-				// 	</div>
-				// <div className="col prevCurrentComparison">
-				// 	{previousSVGs.length > 0 && previousSVGs[index] && (
-				// 		<>
-				// 			<div>
-				// 				<Icon
-				// 					element={el}
-				// 					altText={props.files[index]}
-				// 				/>
-				// 			</div>
-				// 			<div
-				// 				className="previousICONCompare"
-				// 				dangerouslySetInnerHTML={{
-				// 					__html: previousSVGs[index],
-				// 				}}
-				// 			/>
-				// 		</>
-				// 	)}
-				// </div>
-				// </div>
-				// 	)
+				);
 			})}
 		</div>
-	)
-}
+	);
+};
